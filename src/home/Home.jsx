054fd29dbd3c1a082dab628b5a56d3336b4e2fe9
TaxMapTechology <taxmap.taxtech@gmail.com>
2024-01-29 09:32:30 -0300

    update icon site

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -7,7 +7,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import 'swiper/css/navigation'; 
 
 import './home.css';
 
