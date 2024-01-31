32e65020e081b2aa0d6299f220dd97c0fa19f720
TaxMapTechology <taxmap.taxtech@gmail.com>
2024-01-31 16:03:32 -0300

    banner institucional

--- src/pages/Institucional.jsx
+++ src/pages/Institucional.jsx
@@ -1,12 +1,24 @@
 import Header from "../components/Header"
+import './Institucional.css'
+
+// Imagens
+import imgEquipe from '/Imagem_Banner_Pagina_Institucional_TaxMap_Equipe_Reunida.png'
 
 export default function institucional(){
     return(
         <>
             <Header />
-            <section className="banner-institucional">
 
+            <section className="banner-institucional">
+                <div>
+                    <h1>Quase uma década comprometidos em simplificar a vida tributária das empresas.</h1>
+                </div>
+                <div>
+                    <img src={imgEquipe} alt="" />
+                </div>
+               
             </section>
+            <p>Esse objetivo é o que move a TaxMap. Trabalhamos com alto conhecimento e rapidez para simplificar a complexidade tributária, eliminar a burocracia e diminuir riscos para os nossos clientes. </p>
         </>
     )
 }
