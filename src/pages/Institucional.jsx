1db65e726e0c9918d01d78170aa0297aedc15e64
TaxMapTechology <taxmap.taxtech@gmail.com>
2024-02-01 11:23:48 -0300

    update institucional time line

--- src/pages/Institucional.jsx
+++ src/pages/Institucional.jsx
@@ -10,6 +10,10 @@ import imgEquipe from '/Imagem_Banner_Pagina_Institucional_TaxMap_Equipe_Reunida
 import imgSocios from '/socios.png'
 import imgPessoas from '/Imagem_Site_TaxMap_Pessoas_Andando.png'
 
+import logoAntigo from '/Logo_antigo.png'
+import imgLoc from '/Imagem_icone_localizacao.png'
+import imgEquipeTimeLine from '/Imagem_icone_equipe.png'
+
 export default function institucional(){
     return(
         <>
@@ -57,7 +61,60 @@ export default function institucional(){
 
             <img src={imgPessoas} alt="" className="imgPessoas"/>
 
+            {/* Linha do tempo */}
+            <section className="linha-do-tempo">
+                <div className="ano-2014">
+                    <img src={logoAntigo} alt="" className="img-2014"/>
+                    <h1>2014</h1>
+                    <div className="bullet-line">
+                        <div className="bullet"></div>
+                        <hr />
+                    </div>
+                    <p></p>
+                </div>
 
+                <div className="ano-2014">
+                    <img src={imgLoc} alt="" className="img-ano-2016"/>
+                    <h1>2016</h1>
+                    <div className="bullet-line">
+                        <div className="bullet"></div>
+                        <hr />
+                    </div>
+                    <p></p>
+                </div>
+
+                <div className="ano-2014">
+                    <img src={imgEquipeTimeLine} alt="" className="img-ano-2020"/>
+                    <h1>2020</h1>
+                    <div className="bullet-line">
+                        <div className="bullet"></div>
+                        <hr />
+                    </div>
+                    <p></p>
+                </div>
+
+                <div className="ano-2014">
+                    <img src={logoAntigo} alt="" className="img-2014"/>
+                    <h1>2021</h1>
+                    <div className="bullet-line">
+                        <div className="bullet"></div>
+                        <hr />
+                    </div>
+                    <p></p>
+                </div>
+
+                <div className="ano-2014">
+                    <img src={logoAntigo} alt="" className="img-2014"/>
+                    <h1>2023</h1>
+                    <div className="bullet-line">
+                        <div className="bullet"></div>
+                        {/* <hr /> */}
+                    </div>
+                    <p></p>
+                </div>
+            </section>            
+             {/* Linha do tempo */}
+        
 
             <Footer />
         </>
