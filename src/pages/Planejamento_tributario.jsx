c0ae738c0e55ea6312ca750adc7c6b77dfad8632
TaxMapTechology <taxmap.taxtech@gmail.com>
2024-02-02 11:47:11 -0300

    update o que fazemos

--- src/pages/Planejamento_tributario.jsx
+++ src/pages/Planejamento_tributario.jsx
@@ -1,6 +1,11 @@
 import Header from "../components/Header"
 import '../styles/Planejamento_tributario.css'
 
+import { Link } from 'react-router-dom';
+
+// imagens
+import img_planejamento from '/planejamento_tributário.png'
+
 export default function Planejamento_triburario() {
     return(
         <>
@@ -11,9 +16,16 @@ export default function Planejamento_triburario() {
                 <p>O Planejamento Tributário é uma prática fundamental para qualquer empresa ou empreendedor que deseja maximizar seus lucros e manter a saúde financeira do negócio. Consiste em um conjunto de estratégias e ações que visam a otimização do pagamento de tributos, de forma completamente legal e ética.</p>
             </div>
             <div>
-                <img src="" alt="" />
+                <img src={img_planejamento} alt="" />
             </div>
         </section>
+
+        {/* contato */}
+        <section className="contato">
+            <h1>Conte com a TaxMap como sua parceira na busca por eficiência e economia na sua gestão fiscal.</h1>
+            <Link to="" className="Link">Entrar em contato</Link>
+        </section>
+        {/* contato */}
         </>
     )
 }
