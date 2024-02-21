d13e0235a1b923c248c253516131c29f68c948ef
TaxMapTechology <taxmap.taxtech@gmail.com>
2024-02-21 11:17:30 -0300

    add db publicacoes

---
+++ src/pages/publicacoes/data_publi.jsx
@@ -0,0 +1,90 @@
+const data_publi = {
+    publiData: [
+        // BD publicacoes
+        {
+            id: 1,
+            img_banner: '',
+            data_original: '01/12/2023',
+            autor: '',
+            title: 'Publicado acórdão que possibilita o creditamento de ICMS na aquisição de produtos intermediários',
+            corpo: 'Em 11.10.2023, nos autos do EARESP nº 1.775.781/SP, a Primeira Seção, por unanimidade, deu provimento aos embargos de divergência, nos termos do voto da Sra. Ministra Relatora Regina Helena Costa, que entendeu ser cabível o creditamento referente à aquisição de (i) materiais empregados no processo produtivo e (ii) produtos intermediários, inclusive os consumidos ou desgastados gradativamente, desde que comprovada a necessidade de sua utilização para a realização do objeto social da empresa. Dessa forma, o STJ entendeu que as empresas podem ser creditar da aquisição de materiais empregados no processo produtivo, produtos intermediários, inclusive os consumidos ou desgastados gradativamente, desde que comprovada a necessidade de sua utilização para a realização do objeto social da empresa. Nesse ínterim, hoje, 01.12, o acórdão foi devidamente publicado para assentar a prevalência do entendimento jurisprudencial que reconhece, sem a limitação temporal do art. 33, I, da LC n. 87/1996, o direito ao creditamento dos materiais mencionados.',
+            data_atual: '',
+            fonte: '',
+        },
+        {
+            id: 2,
+            img_banner: '',
+            data_original: '04/12/2023',
+            autor: '',
+            title: 'Confaz publica novo convênio para regulamentar os efeitos da ADC 49',
+            corpo: 'O STF, por meio da ADC 49, decidiu que não incide ICMS nas operações interestaduais entre estabelecimentos do mesmo titular. Em sede de embargos de declaração, ficou consignado pela Corte que os Estados podem transferir os créditos gerados nessas operações. Como forma de regulamentação, o Confaz publicou, em 01.11, o Convênio ICMS nº 174, que não foi ratificado pelo Estado de Rio de Janeiro, devido a obrigatoriedade de transferência imposta pela norma regulamentadora, e por isso, foi revogado. Nesse ínterim, em 01.12, sexta-feira, foi publicado o Convênio ICMS nº 178, com o mesmo teor do convênio anterior, pois o entendimento do órgão foi de que a decisão proferida pelo STF tem o condão de transferência obrigatória dos créditos. Cabe ressaltar que, o STF, em momento algum, assinalou a obrigatoriedade da transferência dos créditos nas hipóteses delimitadas. O único ponto disciplinado foi a não incidência ICMS nas operações interestaduais entre estabelecimentos do mesmo titular, cabendo ao Estados a regulamentação do tema.',
+            data_atual: '',
+            fonte: '',
+        },
+        {
+            id: 3,
+            img_banner: '',
+            data_original: '06/12/2023',
+            autor: '',
+            title: 'Congresso aprova lei complementar para dispor sobre ICMS nas transferências',
+            corpo: 'A Câmara dos Deputados aprovou nesta terça-feira (5) projeto de lei complementar que inclui decisão do Supremo Tribunal Federal (STF) na legislação para dispor sobre o pagamento do ICMS a transferência de produtos entre estabelecimentos de uma mesma empresa. A matéria será enviada à sanção presidencial. A questão já havia sido julgada em 2017, mas neste ano, após julgar embargos, o Supremo decidiu que as regras sobre o aproveitamento de créditos do ICMS deveriam ser disciplinadas até o fim do ano, senão seriam integralmente aproveitados pelo contribuinte a partir de 2024. Como não houve acordo unânime no âmbito do Conselho Nacional de Política Fazendária (Confaz), que reúne secretários de Fazenda estaduais, o tema foi tratado pelo Senado no PLP O texto terá vigência a partir do próximo ano e muda a chamada Lei Kandir (Lei Complementar 87/96), prevendo, além da não incidência do imposto na transferência de mercadorias para outro depósito do mesmo contribuinte, que a empresa poderá aproveitar o crédito relativo às operações anteriores, inclusive quando ocorrer transferência interestadual para igual CNPJ. Nesse caso, o crédito deverá ser assegurado pelo estado de destino da mercadoria deslocada por meio de transferência de crédito, mas limitado às alíquotas interestaduais aplicadas sobre o valor atribuído à operação de deslocamento. As alíquotas interestaduais de ICMS são de 7% para operações com destino ao Espírito Santo e estados das regiões Norte, Nordeste e Centro-Oeste; e de 12% para operações com destino aos estados das regiões Sul e Sudeste (exceto Espírito Santo). Se houver diferença positiva entre os créditos anteriores acumulados e a alíquota interestadual, ela deverá ser garantida pela unidade federada de origem da mercadoria deslocada. A fim de evitar que empresas beneficiadas por incentivos fiscais do ICMS deixem de usufruí-los por não pagarem o tributo nessas transferências de mercadorias, o texto permite a elas equiparar a operação àquelas que geram pagamento do imposto, aproveitando o crédito com as alíquotas do estado nas operações internas ou as alíquotas interestaduais nos deslocamentos entre estados diferentes. Todas as medidas valem a partir de 1º de janeiro de 2024. ',
+            data_atual: '',
+            fonte: 'Agência Câmara de Notícias',
+        },
+        {
+            id: 4,
+            img_banner: '',
+            data_original: '13/12/2023',
+            autor: '',
+            title: 'STJ decide que ICMS-ST não compõe a base de cálculo de PIS e Cofins',
+            corpo: 'Essencialmente, o STJ aplicou ao caso a mesma razão de decidir que o Supremo Tribunal Federal usou no Tema 69 da repercussão geral, onde foi fixada a chamada “tese do século”, excluindo o ICMS da base de cálculo do PIS e da Cofins, em 2017. O caso do ICMS-ST chegou a ser debatido pelo STF, que não reconheceu no tema a existência de repercussão geral (RE 1.258.842). A última palavra, assim, foi delegada ao STJ. No caso da substituição tributária, o primeiro agente da cadeia de produção, circulação e consumo de um produto recolhe antecipadamente todo o tributo que seria devido pelos demais contribuintes. Em regra, esse recolhimento recai sobre a indústria ou o importador. Dessa forma, o Fisco tem maior eficiência para cobrar e fiscalizar o imposto. Esse primeiro agente, por sua vez, vai repassar o custo da tributação para os demais integrantes da cadeia, como as redes atacadistas e os comerciantes que atendem ao público. Segundo o ministro Gurgel de Faria, os contribuintes, substituídos ou não, ocupam posições jurídicas idênticas quanto à submissão à tributação pelo ICMS. A única distinção está no mecanismo de recolhimento. Por isso, ele entendeu que a mesma conclusão do STF sobre o ICMS deve ser aplicada pelo STJ ao ICMS-ST. O voto também apontou que a submissão ao regime da substituição tributária depende de lei estadual. Portanto, criar uma distinção entre ICMS regular e ICMS-ST tornaria desigual a arrecadação de PIS e Cofins, tributos de competência federal. Isso faria com que estados e Distrito Federal invadissem a competência tributária da União, além de causar a isenção tributária heterônima — quando um ente federativo isenta imposto cuja competência não lhe pertence. ',
+            data_atual: '',
+            fonte: 'CONJUR',
+        },
+        {
+            id: 5,
+            img_banner: '',
+            data_original: '26/12/2023',
+            autor: '',
+            title: 'Confaz modifica legislação que trata da substituição tributária',
+            corpo: 'Convênio ICMS nº 225 , de 21 de dezembro de 2023 O Conselho Nacional de Política Fazendária - CONFAZ, na sua 386ª Reunião Extraordinária, realizada em Brasília, DF, no dia 21 de dezembro de 2023, tendo em vista o disposto nos arts. 6º a 10 da Lei Complementar nº 87, de 13 de setembro de 1996, nos arts. 102 e 199 do Código Tributário Nacional (Lei nº 5.172, de 25 de outubro de 1966), na alínea "a" do inciso XIII do § 1º e nos §§ 7º e 8º do art. 13, no art. 21-B e nos §§ 12 a 14 do art. 26, todos da Lei Complementar nº 123, de 14 de dezembro de 2006, resolve celebrar o seguinte: CONVÊNIO Cláusula primeira. O § 2º fica acrescido à cláusula décima terceira do Convênio ICMS nº 142, de 14 de dezembro de 2018, com a seguinte redação, renumerando-se o parágrafo único para § 1º: "§ 2º Para efeitos do disposto nesta cláusula e no inciso II da cláusula nona, na hipótese de transferência promovida entre estabelecimentos do remetente, deverá ser deduzido o ICMS destacado na nota fiscal de transferência, nos termos da cláusula quarta do Convênio ICMS nº 178, de 1º de dezembro de 2023. " .Cláusula segunda. Este convênio entra em vigor na data da sua publicação no Diário Oficial da União, produzindo efeitos a partir de 1º de janeiro de 2024.',
+            data_atual: '',
+            fonte: 'COAD',
+        },
+        {
+            id: 6,
+            img_banner: '',
+            data_original: '27/12/2023',
+            autor: '',
+            title: 'ADC 49: São Paulo publica decreto para regulamentar as transferências de mercadorias entre estabelecimentos do mesmo titular',
+            corpo: 'O STF, por meio da ADC 49, decidiu que não incide ICMS nas operações interestaduais entre estabelecimentos do mesmo titular. Em sede de embargos de declaração, ficou consignado pela Corte que os Estados podem transferir os créditos gerados nessas operações. Como forma de regulamentação, o Confaz publicou, em 01.11.2023, o Convênio ICMS nº 174, não ratificado pelo Estado de Rio de Janeiro, uma vez que a norma tornava obrigatória a transferência dos créditos, em desacordo com a decisão do STF. Todavia, em 01.12.2023, foi publicado o Convênio ICMS nº 178, com o mesmo teor do convênio anterior, pois o entendimento do órgão foi de que a decisão proferida pelo STF tem o condão de transferência obrigatória dos créditos. Dito isso, ontem, dia 26.12, o Estado de São Paulo publicou o Decreto nº 68.243, à luz do Convênio ICMS nº 178, para regulamentar a matéria. De acordo com a norma, nas operações interestaduais a aplicação do Convênio é obrigatória, enquanto nas operações internas, é facultativa. Com isso, a partir de 01.01.2024, nas transferências interestaduais de mercadorias entre estabelecimentos de mesma titularidade, os contribuintes paulistas deverão observar o Convênio nº 178. Entretanto, nas operações internas, a adesão ficará a critério do contribuinte e produzirá efeitos por 12 meses. Sobre o tema, importante destacar que, o Projeto de Lei Complementar nº 116/2023, aprovado pela Câmara dos Deputados, para dispor sobre a isenção de pagamento do ICMS na transferência, em linha com a decisão do STF, ainda está pendente de sanção presidencial.',
+            data_atual: '',
+            fonte: '',
+        },
+        {
+            id: 7,
+            img_banner: '',
+            data_original: '28/12/2023',
+            autor: '',
+            title: 'Confaz autoriza negociação com contribuintes',
+            corpo: 'O Conselho Nacional de Política Fazendária (Confaz) permitiu a 13 Estados instituir a chamada transação tributária para a resolução de conflitos envolvendo débitos inscritos na dívida ativa. A autorização está no Convênio ICMS nº 210, publicado recentemente. Os Estados beneficiados são Acre, Alagoas, Amapá, Amazonas, Ceará, Maranhão, Mato Grosso, Pernambuco, Piauí, Rio de Janeiro, Rio Grande do Norte, Rio Grande do Sul e São Paulo.Somente com a publicação desse convênio é que os Estados podem ter segurança para firmar esses acordos com os contribuintes. A Lei Complementar nº 24, de 1975, estabelece, no artigo 1º, que qualquer “favor fiscal” a ser dado por um ente precisa de autorização do Confaz para ter validade. Com o convênio, esses Estados poderão elaborar leis próprias para regulamentar as transações tributárias. O texto também valida a recente Lei nº 17.843, de 2023, do Estado de São Paulo, publicada em novembro, que já traz regras para os acordos. O texto se espelha muito no modelo adotado pela Procuradoria-Geral da Fazenda Nacional (PGFN), que arrecadou, no primeiro semestre, cerca de R$ 10 bilhões. Somente em São Paulo, a estimativa de arrecadação relacionada à transação tributária é de R$ 700 milhões para o próximo ano, segundo dados da Procuradoria-Geral do Estado (PGE-SP). Em 2025, esse montante pode se elevar para R$ 1,5 bilhão e, no ano seguinte, para R$ 2,2 bilhões. De acordo com o convênio do Confaz, as dívidas com esses Estados poderão ser parceladas em até 120 vezes. As multas, juros, demais acréscimos legais e honorários advocatícios poderão ser reduzidos em até 65%. Para o pagamento das dívidas, os contribuintes poderão ainda usar créditos acumulados de ICMS e ICMS-ST e de precatórios. As negociações, porém, só podem envolver créditos classificados como irrecuperáveis ou de difícil recuperação, dívidas de pequeno valor e litígios tributários decorrentes de relevante e disseminada controvérsia jurídica. ',
+            data_atual: '',
+            fonte: 'VALOR ECONÔMICO',
+        },
+        {
+            id: 8,
+            img_banner: '',
+            data_original: '03/01/2023',
+            autor: '',
+            title: 'Pacote Fiscal: Governo Federal edita a Medida Provisória nº 1.202',
+            corpo: 'Informamos que, em 28.12.2023, o Governo Federal editou a Medida Provisória nº 1.202, para introduzir no sistema três medidas centrais: (i) revogação do PERSE (ii) reoneração gradual da folha de pagamento e (iii) limitação à compensação de créditos decorrentes de decisões judiciais transitadas em julgado. Importante destacar que a medida ainda precisa ser submetida à apreciação do Congresso Nacional no prazo de 60 dias, prorrogáveis pelo mesmo período. Revogação do PERSE A norma, em seu artigo 6º, revoga o benefício e estipula, diante do princípio da anterioridade: a cobrança de CSLL, PIS e Cofins a partir de 01.04.2023 e a cobrança do IRPJ a partir de 01.01.2025. Reoneração gradual da folha de pagamento A norma revoga, a partir de 01.04.2024, a Lei 14.784/2023, por meio do qual o Congresso Nacional prorrogou até 2027 a desoneração da folha de pagamentos. É importante destacar que a Lei 14.784 havia sido promulgada apenas um dia antes da edição da Medida Provisória, ora em análise. Anexo 1: refere-se às empresas de setores ferroviários, rodoviários e metroferroviários, transporte escolar e de táxi, televisão e rádio, desenvolvimento e licenciamento de programadas de computador, consultoria, manutenção de serviços e suporte técnico da tecnologia da informação; Anexo 2: contempla as empresas de setores de fabricação de couro e curtimento, artigos para viagem, calçados, e construção de rodovias e ferrovias, obras de arte, urbanização, geração e distribuição de energia elétrica e telecomunicação, redes de abastecimento de água, coleta de esgoto, transportes por dutos, obras de engenharia civil e portuárias, marítimas e fluviais, edição e para impressão de livros, jornais, revistas e produtos gráficos, e atividades de consultoria em gestão empresarial.Importante salientar que, de acordo com o parágrafo único do artigo 1º, as alíquotas reduzidas só serão aplicadas sobre o salário de contribuição do segurado até o valor de um salário-mínimo, aplicando-se a alíquota de 20% sobre o valor que ultrapassar esse limite. Além disso, o artigo 3º exige que as empresas deverão firmar termo no qual se comprometerão a manter, em seus quadros funcionais, quantitativo de empregados igual ou superior ao verificado em 1º de janeiro de cada ano-calendário. Limitação à compensação de créditos Por fim, a norma, em seu artigo 4º, limita a compensação de créditos reconhecidos em decisões judiciais de valores a partir de R$ 10 milhões. De acordo com o texto proposto, o limite mensal não poderá ser inferior a 1/60 avos do valor total do crédito, ou seja 20% ao ano. Além disso, não há menção a um limite máximo estipulado, que dependerá de regulamentação da RFB.',
+            img_corpo: '',
+            data_atual: '',
+            fonte: '',
+        },
+        
+
+    ]
+}
+
+export default data_publi;
