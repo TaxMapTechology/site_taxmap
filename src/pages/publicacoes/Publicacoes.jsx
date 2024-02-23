c6f551b429a119de92f34ce14f3dcf216edee0ed
TaxMapTechology <taxmap.taxtech@gmail.com>
2024-02-23 15:59:15 -0300

    update banner publicacoes

--- src/pages/publicacoes/Publicacoes.jsx
+++ src/pages/publicacoes/Publicacoes.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header'
 
 import confaz from '/confaz.png'
 import acordao from '/publicacoes_img/acordao.png'
+import congresso from '/publicacoes_img/congresso.png'
 
 import { Link } from 'react-router-dom'
 
@@ -64,7 +65,7 @@ export default function Publicacoes(){
                         <!--CARD BEGINING--> */}
                         <div className="blog_card">
                             <a href="" className="figure">
-                                <img src="https://i.postimg.cc/KcwkZQsx/dummy.png" alt="" loading="lazy" />
+                                <img src={congresso} alt="" loading="lazy" />
                                 <span className="tag">13 Dez</span>
                             </a>
                             <section>
