f0d5ee92e2243b01c389bd14f935f4ceeec3f03d
TaxMapTechology <taxmap.taxtech@gmail.com>
2024-03-08 15:07:06 -0300

    update publicacoes

--- src/pages/publicacoes/Publicacoes.jsx
+++ src/pages/publicacoes/Publicacoes.jsx
@@ -21,9 +21,9 @@ export default function Publicacoes(){
             <header className="header">
             <section className="section">
                 <h1 className="title">Publicações TaxMap</h1>
-                <span className="span"><a href="/" className="a"></a> 
+                {/* <span className="span"><a href="/" className="a"></a> 
                 <i className="fa fa_angle_double_right"> </i> 
-                <a href="#" className="a active">Publicações</a></span>
+                <a href="#" className="a active">Publicações</a></span> */}
             </section>  
             </header>
 
