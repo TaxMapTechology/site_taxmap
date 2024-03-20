3dbdd56d8307c7aeef33046580b316032b6b9634
TaxMapTechology <taxmap.taxtech@gmail.com>
2024-03-20 16:44:24 -0300

    update translate site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -90,7 +90,7 @@ export default function Header() {
                         </select>
                     </div>
                     </div>
-            </div>
+                </div>
                          
             </div>
            
